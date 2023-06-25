aab4cf962cf7b6ac1e6d6a3e19ff0b3f3c4546c0
Anton Arbus <anton.arbus@macbookpro>
2023-06-25 21:19:06 +0300

    slide effect

--- nav.js
+++ nav.js
@@ -1,8 +1,13 @@
 function showNavItems() {
   const isNavItemsShown = document.querySelector('.shown-on-phone')
   if (isNavItemsShown) {
-    document.querySelector('.nav-links').classList.remove("shown-on-phone")
+    $(".nav-links").show()
+    $(".nav-links").slideUp(function() {
+      document.querySelector('.nav-links').classList.remove("shown-on-phone")
+    })
   } else {
+    $(".nav-links").hide()
     document.querySelector('.nav-links').classList.add("shown-on-phone")
+    $(".nav-links").slideDown()
   }
 }
