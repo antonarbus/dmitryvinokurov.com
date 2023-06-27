73537b0a502dff082a2bfaf0179489b0641eddd5
Anton Arbus <anton.arbus@macbookpro>
2023-06-27 20:28:18 +0300

    smth

--- nav.js
+++ nav.js
@@ -1,17 +1,9 @@
 function showNavItems() {
-  const navLinks = $(".nav-links")
-  const navItemsShownOnPhone = navLinks.hasClass("shown-on-phone")
-  
-  if (navItemsShownOnPhone) {
-    navLinks.show()
-    navLinks.slideUp(() => {
-      navLinks.removeClass("shown-on-phone")
-      navLinks.removeAttr('style')
-    })
-    return
-  } 
-
-  navLinks.hide()
-  navLinks.addClass("shown-on-phone")
-  navLinks.slideDown()
+  const navLinksOnPhone = $(".phone-nav-links")
+  const isHidden = navLinksOnPhone.is(':hidden')
+  if (isHidden) {
+    navLinksOnPhone.slideDown()
+  } else {
+    navLinksOnPhone.slideUp()
+  }
 }
--- nav_at_index.js
+++
@@ -1,9 +0,0 @@
-function showNavItems() {
-  const navLinksOnPhone = $(".phone-nav-links")
-  const isHidden = navLinksOnPhone.is(':hidden')
-  if (isHidden) {
-    navLinksOnPhone.slideDown()
-  } else {
-    navLinksOnPhone.slideUp()
-  }
-}
